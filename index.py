a206e03502f18a386f75f60bcdfd6d3a2a813265
SamuelGamess123 <138398262+SamuelGamess123@users.noreply.github.com>
2024-02-02 08:37:06 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,27 @@
 import telebot
 import requests
 import os
+import time
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Defina o token do seu bot do Telegram
-TOKEN = '5998033378:AAF7YqOif4jeHZEYtngObD9sX-r6Cs-aCYg'
+TOKEN = os.getenv('TOKEN')
 
 # Cria uma instância do bot
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TOKEN, threaded=False)
+app = Flask(__name__)
+@app.route("/", methods=['POST'])
+    def index():
+        bot.remove_webhook()
+        time.sleep(1)
+        bot.set_webhook()
+        update = telebot.types.Update.de_json(
+            request.stream.read().decode("utf-8"))
+        bot.process_new_updates([update])
+        return "ok", 200
 
 # Função para lidar com o comando /start
 @bot.message_handler(commands=['start'])
@@ -21,16 +36,12 @@ def handle_text_message(message):
         file_url = text
         response = requests.get(file_url)
         if response.status_code == 200:
-            with open(os.path.basename(file_url), 'wb') as file:
+            with open("download.html", 'wb') as file:
                 file.write(response.content)
-            with open(os.path.basename(file_url), 'rb') as file:
+            with open("download.html", 'rb') as file:
                 bot.send_document(message.chat.id, file)
-            os.remove(os.path.basename(file_url))
+            os.remove("download.html")
         else:
-            bot.reply_to(message, 'Falha ao baixar o arquivo HTML.')
+            bot.reply_to(message, f'Falha ao baixar o arquivo HTML, seu código de retorno é: {response.status_code}')
     else:
         bot.reply_to(message, 'Por favor, envie um link válido para um arquivo HTML.')
-
-# Inicia o bot
-bot.remove_webhook()
-bot.set_webhook(url="https://python-bot-telegram-a9hi2qsj6-samuelgamess123.vercel.app/")
